bac8fb1450dd442b1a90e8cf6b4d298a55b79247
EricWijaya96 <ericwi1996@gmail.com>
2016-12-01 17:29:30 +0700

    Memasukkan Tugas 6
    Saya memasukkan tugas ini berdasarkan aturan dan ketentuan yang berlaku.

---
+++ Tugas #6/application/config/routes.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$route['hallo'] = 'hallo';
+$route['EricWi'] = 'EricWi';
+$route['Biodata'] = 'Biodata';
+$route['UKMku'] = 'UKMku';
+$route['Ceritaku'] = 'Ceritaku';
+$route['Plus'] = 'Plus';
+
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
